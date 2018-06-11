01ef5a5d114b86b6239520f2482805863b81b3f0
X-wenhao <c.x.wenhao@gmail.com>
2018-06-11 22:15:46 +0800

    remove draw from division

--- relaxrender/SubdivisionSurface/CatmullClarkSubdivision.py
+++ relaxrender/SubdivisionSurface/CatmullClarkSubdivision.py
@@ -5,12 +5,6 @@
 from relaxrender.triangle import Triangle,Triangles
 from relaxrender.points import Point,Point3D
 
-
-from mpl_toolkits.mplot3d import Axes3D  
-from mpl_toolkits.mplot3d.art3d import Poly3DCollection,Line3DCollection
-import matplotlib.pyplot as plt
-import numpy as np
-
 #__all__==['CatmullClarkSubdivision']
 
 def  CatmullClarkSubdivision(faces,num):
@@ -32,44 +26,6 @@ def  CatmullClarkSubdivision(faces,num):
         #print(line_to_faces,face_to_lines)
     points,point_to_lines,line_to_points=build_index(lines)
     #draw(face_to_lines,line_to_points,points)
-
-def draw(face_to_lines,line_to_points,points):
-    verts=list((p.data[0],p.data[1],p.data[2]) for p in points)
-    faces=[]
-    #print(face_to_lines,line_to_points)
-    for ftl in face_to_lines.values():
-        faces.append([])
-        faces[-1].extend(line_to_points[ftl[0]])
-        tmp=[ftl[0]]
-        for i in range(2):
-            for ltp in ftl:
-                #print(ftl,ltp)
-                if ltp not in tmp and faces[-1][-1] in line_to_points[ltp]:
-                    faces[-1].append(line_to_points[ltp][1-line_to_points[ltp].index(faces[-1][-1])])
-                    tmp.append(ltp)
-    #print(verts,'\n',faces)         
-        
-
-    poly3d = [[verts[vert_id] for vert_id in face] for face in faces]  
-    # print(poly3d)  
-    fig = plt.figure()  
-    ax = fig.gca(projection='3d')
-    # 绘制顶点  
-    x, y, z = zip(*verts)  
-    ax.scatter(x, y, z)  
-    # 绘制多边形面  
-    ax.add_collection3d(Poly3DCollection(poly3d, facecolors='w', linewidths=1, alpha=0.3))  
-    # 绘制对变形的边  
-    ax.add_collection3d(Line3DCollection(poly3d, colors='k', linewidths=0.5, linestyles=':'))  
-    
-     # 设置图形坐标范围  
-    ax.set_xlabel('X')  
-    ax.set_xlim3d(-0.5, 1.5)  
-    ax.set_ylabel('Y')  
-    ax.set_ylim3d(-0.5, 1.5)  
-    ax.set_zlabel('Z')  
-    ax.set_zlim3d(-0.5, 1.5)  
-    plt.show()
 
 
 def build_index(faces):
@@ -155,7 +111,7 @@ def CatmullClarkSubdivision_in(faces,lines,points,face_to_lines,line_to_points,l
                     for ep in eps:
                         re_faces[-1].extend([ [face_points[i],ep],[points[k],ep] ])
                     tmp.append(k)
-    print(len(re_faces),'\n',re_faces[0])
+    #print(len(re_faces),'\n',re_faces[0])
     return re_faces
 
 
