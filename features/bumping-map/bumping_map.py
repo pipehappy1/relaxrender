d1d1136db8411c019c7a80ff02d59f76ea3a55f0
十木 <yangwx0905@sina.com>
2018-06-17 23:13:23 +0800

    修改2.0

--- features/bumping-map/bumping_map.py
+++ features/bumping-map/bumping_map.py
@@ -11,4 +11,37 @@ def extract(cond, x):
     else:
         return np.extract(cond, x)
 
+class vec3():
+    def __init__(self, x, y, z):
+        (self.x, self.y, self.z) = (x, y, z)
+    def __mul__(self, other):
+        return vec3(self.x * other, self.y * other, self.z * other)
+    def __add__(self, other):
+        return vec3(self.x + other.x, self.y + other.y, self.z + other.z)
+    def __sub__(self, other):
+        return vec3(self.x - other.x, self.y - other.y, self.z - other.z)
+    def dot(self, other):
+        return (self.x * other.x) + (self.y * other.y) + (self.z * other.z)
+    def __abs__(self):
+        return self.dot(self)
+    def norm(self):
+        mag = np.sqrt(abs(self))
+        return self * (1.0 / np.where(mag == 0, 1, mag))
+    def components(self):
+        return (self.x, self.y, self.z)
+    def extract(self, cond):
+        return vec3(extract(cond, self.x),
+                    extract(cond, self.y),
+                    extract(cond, self.z))
+    def place(self, cond):
+        r = vec3(np.zeros(cond.shape), np.zeros(cond.shape), np.zeros(cond.shape))
+        np.place(r.x, cond, self.x)
+        np.place(r.y, cond, self.y)
+        np.place(r.z, cond, self.z)
+        return r
+rgb = vec3
 
+(w, h) = (800, 600)         #图片大小
+L = vec3(0, 0.9, 1)        #光源位置
+E = vec3(0., 0.35, -1)     #视野位置
+FARAWAY = 1.0e39            #无限长度
