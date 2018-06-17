f2865abea599444b1a4c849487bc038a375fcf0c
LWJ312 <li_weijie@hust.edu.cn>
2018-06-17 23:49:30 +0800

    change some bugs

--- features/gamma_correction/gamma_test.py
+++
@@ -1,9 +0,0 @@
-import test3 as gamma
-from skimage import data,io
-import matplotlib.pyplot as plt
-import numpy as np
-
-#读取图片
-img0 = io.imread('1.png')
-
-img = gamma.gamma_correction(img0,0.3)
---
+++ features/gamma_correction/test_gamma.py
@@ -0,0 +1,18 @@
+import unittest
+
+import gamma_correction as gamma
+from skimage import data,io
+import matplotlib.pyplot as plt
+import numpy as np
+
+class Test_gamma_correction(unittest.TestCase):
+	
+	def test_gamma_correction(self):
+		img0 = io.imread('1.png')
+		img11 = io.imread('11.png')
+		img1 = gamma.Gamma_correction().gamma_correction(img0,0.3)
+		
+		self.assertEqual(img1.any(),img11.any())
+
+if __name__=='__main__':
+	unittest.main()
