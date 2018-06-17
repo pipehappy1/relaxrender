810c3ba2dfd2e787a3962737e3b285a7eb4768ea
707554736 <31764323+707554736@users.noreply.github.com>
2018-06-18 00:24:31 +0800

    Update gamma_correction.py

--- features/gamma_correction/gamma_correction.py
+++ features/gamma_correction/gamma_correction.py
@@ -1,7 +1,7 @@
 ﻿from skimage import data,io
 import matplotlib.pyplot as plt
 import numpy as np
-#I am sorry fofr what I've done
+#the end
 class Gamma_correction():
 
 	def gamma_correction(self,img,gamma):
