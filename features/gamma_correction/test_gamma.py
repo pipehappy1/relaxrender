daed69a53e2da2e87a2df39761f8659be09cc2e6
wqqpcy <540436756@qq.com>
2018-06-17 17:12:54 +0800

    wqq add readme and change code

--- features/gamma_correction/test_gamma.py
+++ features/gamma_correction/test_gamma.py
@@ -1,3 +1,4 @@
+#unittest为测试用的库
 import unittest
 
 import gamma_correction as gamma
@@ -7,12 +8,18 @@ import numpy as np
 
 class Test_gamma_correction(unittest.TestCase):
 	
+	#测试gamma_correction函数功能
 	def test_gamma_correction(self):
+		
 		img0 = io.imread('1.png')
+		#矫正后的标准图片
 		img11 = io.imread('11.png')
+		#gamma_correction矫正所得图片
 		img1 = gamma.Gamma_correction().gamma_correction(img0,0.3)
 		
+		#比较两个图片是否相同
 		self.assertEqual(img1.any(),img11.any())
 
+#主函数
 if __name__=='__main__':
 	unittest.main()
