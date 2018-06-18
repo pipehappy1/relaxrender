97bfbc3814dd1ac227e8d3ffbe5a00f3c46a1128
akah <akacircus@163.com>
2018-06-18 23:42:32 +0800

    test

--- features/gamma_correction/gamma_correction.py
+++ features/gamma_correction/gamma_correction.py
@@ -5,7 +5,7 @@ import numpy as np
 class Gamma_correction():
 
 	def gamma_correction(self,img,gamma):
-		#具体做法先归一化到1，然后gamma作为指数值求出新的像素值再还原-------gamma矫正算法
+		#具体做法先归一化到1，然后gamma作为指数值求出新的像素值再还原---------gamma矫正算法。
 		gamma_table = [np.power(x/255.0,gamma)*255.0 for x in range(256)]
 		gamma_table = np.round(np.array(gamma_table)).astype(np.uint8)
 	
@@ -16,7 +16,7 @@ class Gamma_correction():
 		
 		for x in range(img_size[0]):
 			for y in range(img_size[1]):
-				#像素点的（0，1，2）通道中分别为该点的r,g,b像素值，通过gamma_table矫正其像素值实现gamma矫正
+				#像素点的（0，1，2）通道中分别为该点的r,g,b像素值，通过gamma_table矫正其像素值实现gamma矫正。
 				#R
 				img1[x,y,0] = gamma_table[img[x,y,0]]
 				#G
@@ -33,7 +33,7 @@ class Gamma_correction():
 		ax2.imshow(img1)
 		ax2.set_title("gamma = %f" %gamma)
 		
-		#调整三个子图的间距，默认适应
+		#调整三个子图的间距，默认适应。
 		plt.tight_layout()
 		plt.show()
 	
