47045aaa369e9d3f3fe16eee924c5a7c637d06dc
crystal313 <1027184987@qq.com>
2018-06-18 12:09:49 +0800

    add some

--- features/gamma_correction/test_gamma.py
+++ features/gamma_correction/test_gamma.py
@@ -4,9 +4,10 @@ import gamma_correction as gamma
 from skimage import data,io
 import matplotlib.pyplot as plt
 import numpy as np
-
+#gamma校正测试
 class Test_gamma_correction(unittest.TestCase):
 	
+    #对比测试
 	def test_gamma_correction(self):
 		img0 = io.imread('1.png')
 		img11 = io.imread('11.png')
