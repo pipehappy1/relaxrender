8663b5b2eacd54255962c1c072734a151e3bf152
CargoH <294233249@qq.com>
2018-06-22 22:34:10 +0800

    Signed-off-by: CargoH <294233249@qq.com>

--- features/depth_of_field/depth_of_field/DOF.py
+++ features/depth_of_field/depth_of_field/DOF.py
@@ -5,3 +5,10 @@ import sys
 import imageio as io
 from sys import argv
 from scipy import misc
+def DepthOfField(im_path):
+    point=[20,20]
+    img = io.imread(im_path)
+    dimg = de.depth(img,point)
+    after = bl.blur(img,dimg)
+    plt.imshow(after)
+    plt.show()
