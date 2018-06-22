f70a3092d29f079c48fb0e2696d34b9e6f64a481
Isey666 <350432454@qq.com>
2018-06-22 23:44:37 +0800

    Signed-off-by: Isey666 <350432454@qq.com>

--- features/depth_of_field/depth_of_field/getdepth.py
+++ features/depth_of_field/depth_of_field/getdepth.py
@@ -8,3 +8,12 @@ def get_depth(rows,cols,row,col,point,maximum_dep):
     return distance/maximum_distance*maximum_dep
 
 def depth(img,point):
+    dimg = img.copy()
+    dimg = dimg[:, :, :, np.newaxis]
+    rows, cols, a = img.shape
+    #print("%d" % cols)
+    for row_tag in range(rows):
+      for col_tag in range(cols):
+        dimg[row_tag,col_tag,0] = get_depth(rows,cols,row_tag,col_tag,point,200)
+    print("Calculate depth data successful.")
+    return dimg
