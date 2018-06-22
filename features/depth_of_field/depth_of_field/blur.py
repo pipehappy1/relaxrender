6b2c40437f75ec092ae0ae84d3457241fe260de8
pzn62026 <1114332572@qq.com>
2018-06-22 21:25:25 +0800

    、

---
+++ features/depth_of_field/depth_of_field/blur.py
@@ -0,0 +1,23 @@
+from scipy import ndimage
+from numpy import *
+def blur(img,dimg,L=30):
+
+      rows,cols,a=img.shape
+      #r,g,b = img[100,100]
+      #print(r,g,b)
+      blured1 = img.copy()
+      blured2 = img.copy()
+      blured3 = img.copy()
+      blured1[:,:,1] = ndimage.gaussian_filter(img[:,:,1],sigma = 4)
+      blured2[:,:,1] = ndimage.gaussian_filter(img[:,:,1],5)
+      blured3[:,:,1] = ndimage.gaussian_filter(img[:,:,1],6)
+      for k in range(rows):
+        for j in range(cols):             
+          if dimg[k,j,0] > 3*L:
+            img[k,j] = blured3[k,j]
+          elif dimg[k,j,0] > 2*L:               
+            img[k,j] = blured2[k,j]
+          elif dimg[k,j,0] > L:
+            img[k,j] = blured1[k,j]
+      print("Blur successful!")
+      return img
