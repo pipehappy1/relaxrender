a4f98cdc28690d5b501e9c607e0b487c7869d939
pzn62026 <1114332572@qq.com>
2018-06-23 12:54:48 +0800

    fix a bug

--- features/depth_of_field/test_dof.py
+++ features/depth_of_field/test_dof.py
@@ -3,6 +3,6 @@ from depth_of_field import DOF
 class TestDepthOfField(unittest.TestCase):
     
     def testDepthOfField(self):
-       DOF.DepthOfField("./depth_of_field/timg-2.jpweg")
+       DOF.DepthOfField("./depth_of_field/timg-2.jpeg")
 if __name__ == '__main__':
     unittest.main()
