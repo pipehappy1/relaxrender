1e6dda0caaf0c2b523edeeaae08ce4ebcea505c5
blueelphant <39620689+blueelphant@users.noreply.github.com>
2018-06-25 15:06:36 +0800

    Update objloader.py

--- features/bunny/objloader.py
+++ features/bunny/objloader.py
@@ -1,43 +1,6 @@
 import numpy as np
-import pygame, OpenGL
-from pygame.locals import *
 from OpenGL.GL import *
-from OpenGL.GLU import *
-from OpenGL.GLUT import *
-from OpenGL.GLUT.freeglut import *
 
-
-
-
-def MTL(fdir,filename):
-    contents = {}
-    mtl = None
-    for line in open(fdir+filename, "r"):
-        if line.startswith('#'): continue
-        values = line.split()
-        if not values: continue
-        if values[0] == 'newmtl':
-            mtl = contents[values[1]] = {}
-        elif mtl is None:
-            raise ValueError( "mtl file doesn't start with newmtl stmt" )
-        elif values[0] == 'map_Kd':
-            # load the texture referred to by this declaration
-            mtl[values[0]] = values[1]
-            surf = pygame.image.load(fdir+mtl['map_Kd'])
-            image = pygame.image.tostring(surf, 'RGBA', 1)
-            ix, iy = surf.get_rect().size
-            texid = mtl['texture_Kd'] = glGenTextures(1)
-            glBindTexture(GL_TEXTURE_2D, texid)
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
-                GL_LINEAR)
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,
-                GL_LINEAR)
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, ix, iy, 0, GL_RGBA,
-                GL_UNSIGNED_BYTE, image)
-        else:
-
-            mtl[values[0]] = [ float(x) for x in values[1:4]]
-    return contents
 
 class OBJ:
     def __init__(self, fdir, filename, swapyz=False):
@@ -46,16 +9,16 @@ class OBJ:
         self.normals = []
         self.texcoords = []
         self.faces = []
-
-        self.mtl=[]
-
-        material = None
+        
+        #循环
         for line in open(fdir+filename, "r"):
-            if line.startswith('#'): continue
+            if line.startswith('#'):
+                continue
             values = line.split()
-            if not values: continue
+            if not values:
+                continue
             if values[0] == 'v':
-                v=[ float(x) for x in values[1:4]]
+                v =[float(x) for x in values[1:4]]
                 if swapyz:
                     v = v[0], v[2], v[1]
                 self.vertices.append(v)
@@ -64,36 +27,22 @@ class OBJ:
                 if swapyz:
                     v = v[0], v[2], v[1]
                 self.normals.append(v)
-            elif values[0] == 'vt':
-                v = [float(x) for x in values[1:3]]
-
-                self.texcoords.append(v)
-            elif values[0] in ('usemtl', 'usemat'):
-                material = values[1]
-            elif values[0] == 'mtllib':
-                self.mtl = [fdir,values[1]]
             elif values[0] == 'f':
                 face = []
                 texcoords = []
                 norms = []
                 for v in values[1:]:
-
                     w = v.split('/')
                     face.append(int(w[0]))
-                    if len(w) >= 2 and len(w[1]) > 0:
-                        texcoords.append(int(w[1]))
-                    else:
-                        texcoords.append(0)
                     if len(w) >= 3 and len(w[2]) > 0:
                         norms.append(int(w[2]))
-                    else:
-                        norms.append(0)
-                self.faces.append((face, norms, texcoords, material))
-
+                self.faces.append((face, norms, texcoords))
+    #函数
+    
     def create_bbox(self):
         ps=np.array(self.vertices)
-        vmin=ps.min(axis=0)
-        vmax=ps.max(axis=0)
+        vmin =ps.min(axis=0)
+        vmax =ps.max(axis=0)
 
         self.bbox_center=(vmax+vmin)/2
         self.bbox_half_r=np.max(vmax-vmin)/2
@@ -107,17 +56,14 @@ class OBJ:
         glFrontFace(GL_CCW)
 
         for face in self.faces:
-            vertices, normals, texture_coords, material = face
-
+            vertices, normals, texture_coords = face
             glBindTexture(GL_TEXTURE_2D,1)
 
             glBegin(GL_POLYGON)
             for i in range(len(vertices)):
                 if normals[i] > 0:
                     glNormal3fv(self.normals[normals[i] - 1])
-                if texture_coords[i] > 0:
-                    glTexCoord2fv(self.texcoords[texture_coords[i] - 1])
                 glVertex3fv(self.vertices[vertices[i] - 1])
             glEnd()
         glDisable(GL_TEXTURE_2D)
-        glEndList()
+glEndList()
