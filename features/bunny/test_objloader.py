a6b0b0673da728e4cc36d69bee3df7aa0070f8c6
zz313147877 <39624668+zz313147877@users.noreply.github.com>
2018-06-25 11:49:13 +0800

    Update test_objloader.py

--- features/bunny/test_objloader.py
+++ features/bunny/test_objloader.py
@@ -1,9 +1,13 @@
 import unittest
 import features.bunny.objloader as ol
-
+import pygame
+from pygame.locals import *
 
 class TestObjLoader(unittest.TestCase):
     def test_OBJ(self):
-        obj = ol.OBJ("d:/relaxrender-bgroup/features/bunny/", "bunny.obj", swapyz=True)
+        obj = ol.OBJ("", "bunny.obj", swapyz=True)
         obj.create_bbox()
+        pygame.init()
+        viewport = (600, 600)
+        pygame.display.set_mode(viewport, OPENGL | DOUBLEBUF)
         obj.create_gl_list()
