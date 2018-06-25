2cd9ccf6ec4ad677653048c6e38e8ebac31b481c
moltres <25852469@qq.com>
2018-06-25 13:27:47 +0800

    121

--- features/stochastic_ray_tracing/somefunc/basefunc.py
+++ features/stochastic_ray_tracing/somefunc/basefunc.py
@@ -1,19 +1,1 @@
-#取小数部分
-# def fract(input):
-#     output=input-int(input)
-#     if output>0:
-#         return output
-#     else:
-#         return (1+output)
-
-#把数据限制在0-1
-def clamp(input):
-    if input<0.:
-        return 0
-    elif input<1.:
-        return input
-    else:
-        return 1
-
-
 
